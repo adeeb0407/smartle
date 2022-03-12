437e20136cf802488f0cae3eb9f78764e5bcc9d4
rajan baliwal <rajan18baliwal@gmail.com>
2022-03-12 17:59:37 +0530

    moduleforcoursesAPI

--- server/src/controller/coursesController.ts
+++ server/src/controller/coursesController.ts
@@ -54,3 +54,15 @@ export const getAllCoursesOnHome =  ((req: Request, res: Response) => {
         res.send(rows);
     });
 });
+
+export const getModuleforCourse =  ((req: Request, res: Response) => {
+    let course_id = req.params.id;
+    let sql = `SELECT * FROM module JOIN coursemodule ON module.module_id = coursemodule.module_id AND coursemodule.course_id=${course_id}`;
+    db.query(sql, (err: any, rows: any) =>{
+        if(err){
+            console.log(err);
+        }
+
+        res.send(rows);
+    });
+});
--- server/src/routes/coursesRoutes.ts
+++ server/src/routes/coursesRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import {getAllCourses, getAllCoursesOnHome} from '../controller/coursesController';
+import {getAllCourses, getAllCoursesOnHome, getModuleforCourse} from '../controller/coursesController';
 
 const router = express.Router();
 
 router.get("/course", getAllCourses);
 router.get("/coursesonhome", getAllCoursesOnHome);
+router.get("/getmoduleforcourse/:id", getModuleforCourse);
 
 export default router;
